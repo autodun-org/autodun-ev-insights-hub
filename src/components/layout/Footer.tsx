fb2d927c00919c4757671769a511c4a6d113fdab
kamrangul87 <kamrangul87@gmail.com>
2025-12-02 10:04:32 +0000

    Update Footer.tsx

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -23,11 +23,10 @@ const Footer = () => {
               to="/"
               className="flex items-center transition-smooth hover:opacity-80"
             >
-              {/* A-icon used as first letter of Autodun */}
               <img
                 src={logoIconLight}
                 alt="Autodun"
-                className="mr-[2px] h-6 w-6 shrink-0"
+                className="h-6 w-6 shrink-0 -mr-[2px]" // same behaviour as header
               />
               <span className="text-xl font-semibold tracking-tight leading-none text-foreground">
                 utodun
